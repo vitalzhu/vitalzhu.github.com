189b21671d2afbe23271d27d8c155dc3fdc67ca0
vital <vitalzhu@gmail.com>
2019-01-16 23:30:54 +0800

    Site updated: 2019-01-16 23:30:53

--- js/script.js
+++ js/script.js
@@ -1,82 +1,93 @@
-(function ($) {
-    $('.navbar-burger').click(function () {
-        $(this).toggleClass('is-active');
-        $('.navbar-main .navbar-start').toggleClass('is-active');
-        $('.navbar-main .navbar-end').toggleClass('is-active');
+(function (a) {
+  "use strict";
+
+  var appendStylesheet = function (url) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.body.appendChild(link);
+  };
+
+  appendStylesheet('//cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css');
+
+  var $ = a;
+  $('.post-block p img').each(function () {
+    $(this).wrap('<a data-fancybox="images" href="' + this.src + '" data-caption=" ' + this.alt + '" class="fancybox"></a>');
+    $(this).attr('src', this.src + '?imageMogr2/thumbnail/300x300/gravity/North/crop/200x200');
+  });
+
+  if ($.fancybox) {
+    $('.fancybox').fancybox({
+      thumbs: {
+        // showOnStart: true
+      },
+      caption: function (instance, item) {
+        var caption, link;
+        if (item.type === 'image') {
+          caption = $(this).data('caption');
+          link = '<a href="' + item.src + '">Download</a>';
+          return (caption ? caption : '') + link;
+        }
+      },
     });
+  }
 
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('.navbar-main').outerHeight();
+  // if ($.fancybox) {
+  //   $('.post-block p img')
+  //     .on('click', function () {
+  //       $.fancybox.open([{
+  //         src: this.src,
+  //         opts: {
+  //           caption: '<a href="' + this.src + '" target="_blank" download>Download</a>'
+  //         }
+  //       }]);
+  //     });
+  //   // $.fancybox.defaults.speed = 1000;
+  // }
 
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
 
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta) {
-            return;
-        }
-
-        // If they scrolled down and are past the navbar, add class .navbar-down.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight) {
-            var posY = Math.min(st, navbarHeight);
-            // Scroll Down
-            $('.navbar-main').css({
-                '-webkit-transform' : 'translateY(-' + posY + 'px)',
-                '-moz-transform'    : 'translateY(-' + posY + 'px)',
-                '-ms-transform'     : 'translateY(-' + posY + 'px)',
-                '-o-transform'      : 'translateY(-' + posY + 'px)',
-                'transform'         : 'translateY(-' + posY + 'px)'
-            });
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                $('.navbar-main').css({
-                    '-webkit-transform' : 'translateY(0px)',
-                    '-moz-transform'    : 'translateY(0px)',
-                    '-ms-transform'     : 'translateY(0px)',
-                    '-o-transform'      : 'translateY(0px)',
-                    'transform'         : 'translateY(0px)'
-                });
-            }
-        }
-
-        lastScrollTop = st;
+  function b(a) {
+    var c, d, b = [];
+    if ("number" == typeof a) b.push(a);
+    else {
+      d = a.split(",");
+      for (var e = 0; e < d.length; e++)
+        if (c = d[e].split("-"), 2 === c.length)
+          for (var f = parseInt(c[0], 10); f <= c[1]; f++) b.push(f);
+        else 1 === c.length && b.push(parseInt(c[0], 10))
     }
-
-    $('.article.gallery img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="caption">' + this.alt + '</div>');
-            }
+    return b
+  }
+  a.fn.gist = function (c) {
+    return this.each(function () {
+      var e, f, g, h, i, j, k, l, m, n, d = a(this),
+        o = {};
+      return d.css("display", "block"), e = d.data("gist-id") || "", g = d.data("gist-file"), k = d.data("gist-hide-footer") === !0, l = d.data("gist-hide-line-numbers") === !0, h = d.data("gist-line"), j = d.data("gist-highlight-line"), n = d.data("gist-show-spinner") === !0, m = n ? !1 : void 0 !== d.data("gist-show-loading") ? d.data("gist-show-loading") : !0, g && (o.file = g), e ? (f = "https://gist.github.com/" + e + ".json", i = "Loading gist " + f + (o.file ? ", file: " + o.file : "") + "...", m && d.html(i), n && d.html('<img style="display:block;margin-left:auto;margin-right:auto"  alt="' + i + '" src="https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif">'), void a.ajax({
+        url: f,
+        data: o,
+        dataType: "jsonp",
+        timeout: 2e4,
+        success: function (c) {
+          var e, g, i, m, n;
+          c && c.div ? (c.stylesheet && (0 === c.stylesheet.indexOf("<link") ? c.stylesheet = c.stylesheet.replace(/\\/g, "").match(/href=\"([^\s]*)\"/)[1] : 0 !== c.stylesheet.indexOf("http") && (0 !== c.stylesheet.indexOf("/") && (c.stylesheet = "/" + c.stylesheet), c.stylesheet = "https://gist.github.com" + c.stylesheet)), c.stylesheet && 0 === a('link[href="' + c.stylesheet + '"]').length && (e = document.createElement("link"), g = document.getElementsByTagName("head")[0], e.type = "text/css", e.rel = "stylesheet", e.href = c.stylesheet, g.insertBefore(e, g.firstChild)), n = a(c.div), n.removeAttr("id"), d.html("").append(n), j && (m = b(j), n.find("td.line-data").css({
+            width: "100%"
+          }), n.find(".js-file-line").each(function (b) {
+            -1 !== a.inArray(b + 1, m) && a(this).css({
+              "background-color": "rgb(255, 255, 204)"
+            })
+          })), h && (i = b(h), n.find(".js-file-line").each(function (b) {
+            -1 === a.inArray(b + 1, i) && a(this).parent().remove()
+          })), k && (n.find(".gist-meta").remove(), n.find(".gist-data").css("border-bottom", "0px"), n.find(".gist-file").css("border-bottom", "1px solid #ddd")), l && n.find(".js-line-number").remove()) : d.html("Failed loading gist " + f)
+        },
+        error: function (a, b) {
+          d.html("Failed loading gist " + f + ": " + b)
+        },
+        complete: function () {
+          "function" == typeof c && c()
         }
-    });
-
-    $('.article-entry').find('h1, h2, h3, h4, h5, h6').on('click', function () {
-        if ($(this).get(0).id) {
-            window.location.hash = $(this).get(0).id;
-        }
-    });
-
-    if (typeof(moment) === 'function') {
-        $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
-    }
+      })) : !1
+    })
+  }, a(function () {
+    a("[data-gist-id]").gist()
+  })
 })(jQuery);
